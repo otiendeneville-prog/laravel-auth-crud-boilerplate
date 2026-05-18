4e00102165c0587b8e987e7e8eb7e48c24d9d014
Neville <otiendeneville@gamil.com>
2026-05-18 14:17:28 +0300

    using @foreach to view ideas

--- resources/views/ideas.blade.php
+++ resources/views/ideas.blade.php
@@ -52,12 +52,13 @@
             @else
             @endif
             <div class=" mt-6 grid grid-col-2 gap-6-xl gap-y-4 card bg-neutral text-neutral-content w-96">
+                 @foreach($ideas as $idea)
                 <div class="card-body items-center text-center">
-                    <h2 class="card-title">Cookies!</h2>
-                    <p>We are using cookies for no reason.</p>
+                    <h2 class="card-title">{{$idea->description}}</h2>
                     <div class="card-actions justify-end">
                     </div>
                 </div>
+                  @endforeach
             </div>
             <div class=" mt-6 grid grid-col-2 gap-6-xl gap-y-4 card bg-neutral text-neutral-content w-96">
                 <div class="card-body items-center text-center">
