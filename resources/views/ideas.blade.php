c9e0a46ceef9fc696fb2c5f77b772dd89451e272
Neville <otiendeneville@gamil.com>
2026-05-18 14:05:51 +0300

    additing of cookie buttons

--- resources/views/ideas.blade.php
+++ resources/views/ideas.blade.php
@@ -67,16 +67,7 @@
                     </div>
                 </div>
             </div>
-            <div class=" mt-6 grid grid-col-2 gap-6-xl gap-y-4 card bg-neutral text-neutral-content w-96">
-                <div class="card-body items-center text-center">
-                    <h2 class="card-title">Cookies!</h2>
-                    <p>We are using cookies for no reason.</p>
-                    <button class="btn btn-primary">Accept</button>
-                    <button class="btn btn-ghost">Deny</button>
-                    <div class="card-actions justify-end">
-                    </div>
-                </div>
-            </div>
+         
             <div class=" mt-6 grid grid-col-2 gap-6-xl gap-y-4 card bg-neutral text-neutral-content w-96">
                 <div class="card-body items-center text-center">
                     <h2 class="card-title">Cookies!</h2>
