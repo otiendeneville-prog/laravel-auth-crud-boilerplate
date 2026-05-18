0802b073a3179ef1b6f4e07d6f42eabe85323181
Neville <otiendeneville@gamil.com>
2026-05-18 14:20:43 +0300

    condition of endloop

--- resources/views/ideas.blade.php
+++ resources/views/ideas.blade.php
@@ -58,7 +58,6 @@
                     <div class="card-actions justify-end">
                     </div>
                 </div>
-                  @endforeach
             </div>
             <div class=" mt-6 grid grid-col-2 gap-6-xl gap-y-4 card bg-neutral text-neutral-content w-96">
                 <div class="card-body items-center text-center">
@@ -67,6 +66,7 @@
                     <div class="card-actions justify-end">
                     </div>
                 </div>
+                  @endforeach
             </div>
          
             <div class=" mt-6 grid grid-col-2 gap-6-xl gap-y-4 card bg-neutral text-neutral-content w-96">
@@ -76,6 +76,7 @@
                     <div class="card-actions justify-end">
                     </div>
                 </div>
+                
             </div>
         </div>
 
