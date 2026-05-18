95b01e329e760305eacd0be66fe833b65c937e2f
Neville <otiendeneville@gamil.com>
2026-05-18 13:43:26 +0300

    adding ui deter for the ui

--- app/Http/Requests/storeIdeaRequest.php
+++ app/Http/Requests/storeIdeaRequest.php
@@ -8,9 +8,7 @@ use League\CommonMark\Extension\DescriptionList\Node\Description;
 
 class storeIdeaRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
+   
     public function authorize(): bool
     {
         return true;
