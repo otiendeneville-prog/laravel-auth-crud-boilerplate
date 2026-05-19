8784368d9f8057982551fe8932c70890ab1869b5
Neville <otiendeneville@gamil.com>
2026-05-19 08:03:33 +0300

    validate the request

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -10,8 +10,12 @@ class RegisteredUserController extends Controller
      public function create(){
         return view('register');
     }
-    public function store(){
-        dd('request()->all()');
+    public function store(Request $request){
+       $validate = $request->validate([
+            'name'=>['required','string','max:255'],
+            'email'=>['required','string','email','max:255','inique:users'],
+            'password' =>['required','password::default()'],
+        ]);
     }
 }
 
