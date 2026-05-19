97220ba298c63965b88cfacef59c27212949e68e
Neville <otiendeneville@gamil.com>
2026-05-19 07:21:42 +0300

    accessing for action

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,5 +1,6 @@
 <x-layout>
   register from here:
+  <form action="/register" method="POST">
   <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4 max-auto">
   <legend class="fieldset-legend">Register</legend>
 
@@ -12,6 +13,7 @@
   <label class="label">Password</label>
   <input type="password" class="input" placeholder="Password" />
 
-  <button class="btn btn-neutral mt-4">Login</button>
+  <button class="btn btn-neutral mt-4">Register</button>
 </fieldset>
+</form>
 </x-layout>
