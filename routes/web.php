6481188b9a4c2d914ac2a9881976def1f375e9d6
Neville <otiendeneville@gamil.com>
2026-05-20 09:17:30 +0300

    log out controller session

--- routes/web.php
+++ routes/web.php
@@ -5,26 +5,31 @@ use App\Models\Idea;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IdeaController;
 use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\SessionsController;
+
 
 
 Route::get('/', function () {
     return redirect('/ideas');
 });
 
-Route::get('/ideas',[IdeaController::class,'index']);
+Route::get('/ideas', [IdeaController::class, 'index']);
 //display a single idea;
-Route::get('/ideas/{idea}',[IdeaController::class,'show']); 
+Route::get('/ideas/{idea}', [IdeaController::class, 'show']);
 //storing a new idea;
-Route::post('/ideas', [IdeaController::class,'store']);
+Route::post('/ideas', [IdeaController::class, 'store']);
 
 //update and existing idea;
-Route::patch('/ideas/{idea}', [IdeaController::class,'update']);
-//edit
-Route::get('idea/{idea}',[IdeaController::class,'edit']);
+Route::patch('/ideas/{idea}', [IdeaController::class, 'update']);
+// //edit
+// Route::get('idea/{idea}',[IdeaController::class,'edit']);
+Route::get('/ideas/{idea}/edit', [IdeaController::class, 'edit']);
+
 
 //delete an idea moved outside the
-Route::delete('/ideas/{idea}', [IdeaController::class,'destroy']);
+Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy']);
 
 
-   Route::get('/register',[RegisteredUserController::class,'create']);
-   Route::post('/register',[RegisteredUserController::class,'store']);
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+Route::delete('/logout', [SessionsController::class, 'destroy']);
