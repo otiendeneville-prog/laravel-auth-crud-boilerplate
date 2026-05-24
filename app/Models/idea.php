6782bbe38682339249bc00252c1a725e31e49004
Neville <otiendeneville@gamil.com>
2026-05-24 10:05:21 +0300

    addition of new table

--- app/Models/idea.php
+++ app/Models/idea.php
@@ -1,16 +1,12 @@
 <?php
 
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class idea extends Model
 {
-        use HasFactory;
-    protected $fillable = ['description','state'];
-
+    use HasFactory;
+    protected $table = 'ideas';
 }
-
-
-
-
