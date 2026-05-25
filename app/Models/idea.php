b076ef2ed5784031c24477171ccef21fe75a741d
Neville <otiendeneville@gamil.com>
2026-05-25 09:37:38 +0300

    delete props

--- app/Models/idea.php
+++ app/Models/idea.php
@@ -5,9 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-
-@property-read collecction, idea< $ideas; 
-
 class idea extends Model
 {
     use HasFactory;
