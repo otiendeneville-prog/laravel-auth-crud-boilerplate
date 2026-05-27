c5ebadc11795a668933453a509b70c40c89a4fba
Neville <otiendeneville@gamil.com>
2026-05-27 08:44:03 +0300

    return new ordered sipp

--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,6 @@ Route::get('admin', function(){
 Route::delete('/logout', [SessionsController::class, 'destroy']);
 
 Route::get('/',function(){
-    Mail::send(new\App\Mail\OrderShipped());
 
-    return view('/ideas');
-})
+    return new('ordered-shipped');
+});
