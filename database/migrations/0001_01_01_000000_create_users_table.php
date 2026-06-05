cba8dd4c65f6797a76185f7df4e100063c6e2b67
Neville <otiendeneville@gamil.com>
2026-06-05 08:14:38 +0300

    add status defaultl

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->string('status')->default('pending');
             $table->timestamp();
             $table->json('links')->defeult('[]');
         });
