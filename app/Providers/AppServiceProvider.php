39b449e6fbd3ff5bc7e389b7cdfaf5175e650927
Neville <otiendeneville@gamil.com>
2026-06-06 08:41:34 +0300

    add model unguard

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Dabase\Eloquent\Model;
+use Illuminate\Database\Eloquent\Model;
 
 class AppServiceProvider extends ServiceProvider
 {
